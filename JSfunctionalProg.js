9362608099ca1c6ec96d506405e2334b9091e5cb
WinterCricket <hodhodhod2003@gmail.com>
2018-11-05 21:23:19 -0600

    add js

---
+++ JSfunctionalProg.js
@@ -0,0 +1,6 @@
+function sliceHouses(house, beginHouse, endHouse) {
+  
+  return house.slice(beginHouse, endHouse);
+  
+}
+var inputHouse = ["Mobile Home", "Small Camp", "Single Family", "Multi-Family"];
