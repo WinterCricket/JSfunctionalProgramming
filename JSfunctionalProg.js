773ea396686f3a7cfdedb8d06dfdc8e29bb95b80
WinterCricket <hodhodhod2003@gmail.com>
2018-11-06 23:01:41 -0600

    use methods filter and map

--- JSfunctionalProg.js
+++ JSfunctionalProg.js
@@ -1,6 +1,24 @@
-function sliceHouses(house, beginHouse, endHouse) {
+// function sliceHouses(house, beginHouse, endHouse) {
   
-  return house.slice(beginHouse, endHouse);
+//   return house.slice(beginHouse, endHouse);
   
-}
-var inputHouse = ["Mobile Home", "Small Camp", "Single Family", "Multi-Family"];
+// }
+// var inputHouse = ["Mobile Home", "Small Camp", "Single Family", "Multi-Family"];
+
+var dogs = [
+  {weight: "fat", color: "red"}, 
+  {weight: "thin", color: "blue"}, 
+  {weight: "just right", color: "brown"},
+  {weight: "fat", color: "brown"}
+];
+
+var filterDogs = dogs.filter(function(dog){
+	return dog.color === "brown";
+});
+var newVar = filterDogs.map(function(filtered) {
+	return filtered['weight'];
+  
+});
+
+
+//output is    ["just right","fat"]
