9c4de9629125189f11d161883dbb6451c6a34dd9
WinterCricket <hodhodhod2003@gmail.com>
2018-11-07 20:56:48 -0600

    filter performance by using splice

--- JSfunctionalProg.js
+++ JSfunctionalProg.js
@@ -5,21 +5,26 @@
 // }
 // var inputHouse = ["Mobile Home", "Small Camp", "Single Family", "Multi-Family"];
 
-rest operation and spread…
-const sum = (function() {
+function filteredArray(arr, elem) {
+  let newArr = [...arr];
+  for (let i = 0; i < newArr.length; i++) {
+  for(let j = 0; j< newArr[i].length; j++) {
+  	if(newArr[i][j] === elem) {
+    	newArr.splice(i, 1);
+      i--;
+      break;
+    }
+  }
+ 
+
   
-  "use strict";
-  return function sum(...args) {
-    
-    return args.reduce((a, b) => a + b, 0);
-  };
-  
-})();
-  function howMany(...args) {
-  return "You have passed " + args.length + " arguments.";
 }
-console.log(howMany(0, 1, 2));
-console.log(sum(1, 2, 3)); // 6
+    return newArr;
+}
+
+// change code to change cases:
+filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 12);
+
 
 
 
