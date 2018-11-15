21b3048248fbbdef4468bc475bb94d939aed7344
WinterCricket <hodhodhod2003@gmail.com>
2018-11-14 20:43:56 -0600

    use let...in

--- JSfunctionalProg.js
+++ JSfunctionalProg.js
@@ -5,25 +5,6 @@
 // }
 // var inputHouse = ["Mobile Home", "Small Camp", "Single Family", "Multi-Family"];
 
-function filteredArray(arr, elem) {
-  let newArr = [...arr];
-  for (let i = 0; i < newArr.length; i++) {
-  for(let j = 0; j< newArr[i].length; j++) {
-  	if(newArr[i][j] === elem) {
-    	newArr.splice(i, 1);
-      i--;
-      break;
-    }
-  }
- 
-
-  
-}
-    return newArr;
-}
-
-// change code to change cases:
-filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 12);
 
 
 
